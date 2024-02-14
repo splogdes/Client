513a6004af668a891b15bf2c4780ba0818e5f899
splogdes <95136830+splogdes@users.noreply.github.com>
2024-02-14 00:16:07 +0000

    tabs

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,37 @@
 #include "main.h"
 
 void setup() {
-  Serial.begin(9600);
-  setupWiFiUDP();
+	Serial.begin(9600);
+	delay(100);
+	setupWiFiUDP();
 }
 
 void loop() {
-  JsonDocument doc;
-  int counter = 0;
+	JsonDocument doc;
 
-  while (true) {
-    counter++;
+	while (true) {
 
+		// Send artificial sensor data
+		doc.clear();
+		doc["message_type"] = "sensor_data";
+		doc["mac"] = macAddress;
+		doc["sensors"]["bonsai"]["moisture"] = random(0, 100);
+		doc["sensors"]["room monitor"]["temperature"] = random(-100, 0);
+		doc["sensors"]["room monitor"]["humidity"] = random(0, 200);
 
-    // Send artificial sensor data
-    doc.clear();
-    doc["message_type"] = "sensor_data";
-    doc["mac"] = macAddress;
-    doc["sensors"]["bonsai"]["moisture"] = random(0, 100);
-    doc["sensors"]["room monitor"]["temperature"] = random(-100, 0);
-    doc["sensors"]["room monitor"]["humidity"] = random(0, 200);
+		char buffer[255];
+		serializeJson(doc, buffer);
 
-    char buffer[255];
-    serializeJson(doc, buffer);
+		udp.beginPacket(udp_server, udp_port);
+		udp.write(buffer);
+		udp.endPacket();
 
-    udp.beginPacket(udp_server, udp_port);
-    udp.write(buffer);
-    udp.endPacket();
+		Process_response();
 
-    delay(500);
+		delay(5000);
 
-    Process_response();
-
-    delay(5000);
-
-  }
+	}
 }
 
 
-  
+	
